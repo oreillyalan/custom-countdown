fc0437dd642975f3ad973b798a34a4b64eab33ec
oreillyalan <oreillyalan132@gmail.com>
2020-08-27 10:19:43 +0100

    timer now counts down using setInterval

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const timeElements = countdown.getElementsByTagName('span');
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
+let countdownActive;
+
 
 const second = 1000;
 const minute = second * 60;
@@ -24,24 +26,23 @@ const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute('min', today);
 
 function updateDom() {
-    const now = new Date().getTime();
-    const distance = countdownValue - now;
-    //console.log(now, distance);
-    const days = Math.floor(distance/day);
-    const hours = Math.floor((distance%day) / hour);
-    const minutes = Math.floor((distance%hour) / minute);
-    const seconds = Math.floor((distance%minute) / second);
-    //console.log(days, hours, minutes, seconds);
-    countdownElTitle.textContent = `${countdownTitle}`;
-    // console.log(timeElements[0].textContent);
-
-    timeElements[0].textContent = `${days}`;
-    timeElements[1].textContent = `${hours}`;
-    timeElements[2].textContent = `${minutes}`;
-    timeElements[3].textContent = `${seconds}`;
-
-    inputContainer.hidden = true;   
-    countdownEl.hidden = false;   
+    countdownActive = setInterval(()=> {    
+        const now = new Date().getTime();
+        const distance = countdownValue - now;
+        const days = Math.floor(distance/day);
+        const hours = Math.floor((distance%day) / hour);
+        const minutes = Math.floor((distance%hour) / minute);
+        const seconds = Math.floor((distance%minute) / second);
+        countdownElTitle.textContent = `${countdownTitle}`;
+    
+        timeElements[0].textContent = `${days}`;
+        timeElements[1].textContent = `${hours}`;
+        timeElements[2].textContent = `${minutes}`;
+        timeElements[3].textContent = `${seconds}`;
+    
+        inputContainer.hidden = true;   
+        countdownEl.hidden = false;   
+    }, second);
 
 
 }
@@ -52,9 +53,7 @@ function updateCountdown(e){
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
-    console.log(countdownDate, countdownTitle)
     countdownValue = new Date(countdownDate).getTime();
-    console.log(countdownValue);
     updateDom();
 
         
