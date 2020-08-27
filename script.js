9a0488413926a1199d3c9adb24ab032425aa609a
oreillyalan <oreillyalan132@gmail.com>
2020-08-27 10:46:32 +0100

    new countdown button now resets the countdown

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = countdown.getElementsByTagName('span');
 
-
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeElBtn = document.getElementById('complete-button');
 
 let countdownTitle = '';
 let countdownDate = '';
@@ -33,15 +35,23 @@ function updateDom() {
         const hours = Math.floor((distance%day) / hour);
         const minutes = Math.floor((distance%hour) / minute);
         const seconds = Math.floor((distance%minute) / second);
+
+        inputContainer.hidden = true;
+        if( distance < 0){
+            countdownEl.hidden = true;
+            clearInterval(countdownActive);
+            completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+            completeEl.hidden = false;
+        }
+        else {
         countdownElTitle.textContent = `${countdownTitle}`;
-    
         timeElements[0].textContent = `${days}`;
         timeElements[1].textContent = `${hours}`;
         timeElements[2].textContent = `${minutes}`;
         timeElements[3].textContent = `${seconds}`;
-    
-        inputContainer.hidden = true;   
-        countdownEl.hidden = false;   
+        completeEl.hidden = true;
+        countdownEl.hidden = false; 
+         }     
     }, second);
 
 
@@ -59,6 +69,7 @@ function updateCountdown(e){
 
 function reset(){
     countdownEl.hidden = true;   
+    completeEl.hidden = true;
     inputContainer.hidden = false;   
     clearInterval(countdownActive);
     countdownTitle = '';
@@ -68,3 +79,4 @@ function reset(){
 // Event Listeners
 countdownForm.addEventListener('submit',updateCountdown);
 countdownBtn.addEventListener('click',reset);
+completeElBtn.addEventListener('click',reset);
